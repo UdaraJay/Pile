5d1a915035166d552bfd8ddcd43d41cd3d7afe0b
udara <me@udara.io>
2023-10-27 22:31:20 -0400

    Debounce utility

---
+++ src/renderer/utils/debounce.js
@@ -0,0 +1,11 @@
+const debounce = (func, delay) => {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func(...args);
+    }, delay);
+  };
+};
+
+export default debounce;

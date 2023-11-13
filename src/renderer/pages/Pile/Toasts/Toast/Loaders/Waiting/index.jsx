b4ca798ce52ed358e662d994abbb38032999f6eb
Rus Khakimov <396223+khakimov@users.noreply.github.com>
2023-11-13 15:12:54 +0700

    fix: stroke-opacity to strokeOpacity

--- src/renderer/pages/Pile/Toasts/Toast/Loaders/Waiting/index.jsx
+++ src/renderer/pages/Pile/Toasts/Toast/Loaders/Waiting/index.jsx
@@ -10,7 +10,7 @@ export default function Waiting(props) {
     >
       <g fill="none" fillRule="evenodd">
         <g transform="translate(1 1)" stroke-width="3">
-          <circle stroke-opacity=".5" cx="18" cy="18" r="18" />
+          <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
           <path d="M36 18c0-9.94-8.06-18-18-18">
             <animateTransform
               attributeName="transform"

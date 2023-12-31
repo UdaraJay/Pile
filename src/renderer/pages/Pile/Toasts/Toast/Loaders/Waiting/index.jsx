7d65bb7034705373b7ffd3bbe2154a283fe32daa
udara <me@udara.io>
2023-12-30 22:54:12 -0500

    rebase fix

--- src/renderer/pages/Pile/Toasts/Toast/Loaders/Waiting/index.jsx
+++ src/renderer/pages/Pile/Toasts/Toast/Loaders/Waiting/index.jsx
@@ -11,10 +11,7 @@ export default function Waiting(props) {
       <g fill="none" fillRule="evenodd">
         <g transform="translate(1 1)" strokeWidth="3">
           <circle strokeOpacity=".5" cx="18" cy="18" r="18" />
-<<<<<<< HEAD
 
-=======
->>>>>>> 42b018f (fix: stroke-opacity, stroke-width, stroke-linecap, fill-rule)
           <path d="M36 18c0-9.94-8.06-18-18-18">
             <animateTransform
               attributeName="transform"

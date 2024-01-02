d1f80a1e09286bc5ede19259bfdc9ee3bbd6d854
Sidney Nimako <snibo13@gmail.com>
2024-01-02 02:18:45 -0500

    Update AIContext.js
    Signed-off-by: Sidney Nimako <snibo13@gmail.com>

--- src/renderer/context/AIContext.js
+++ src/renderer/context/AIContext.js
@@ -6,11 +6,15 @@ import {
   useCallback,
 } from 'react';
 import OpenAI from 'openai';
+import ollama from '../../main/utils/ollama';
+// import { set } from 'main/utils/pileLinks';
 
 export const AIContext = createContext();
 
 export const AIContextProvider = ({ children }) => {
   const [ai, setAi] = useState(null);
+  const [model, setModel] = useState('gpt-3');
+  const [type, setType] = useState('openai');
 
   const prompt =
     'You are an AI within a journaling app. Your job is to help the user reflect on their thoughts in a thoughtful and kind manner. The user can never directly address you or directly respond to you. Try not to repeat what the user said, instead try to seed new ideas, encourage or debate. Keep your responses concise, but meaningful.';
@@ -22,17 +26,32 @@ export const AIContextProvider = ({ children }) => {
   const setupAi = async () => {
     const key = await getKey();
 
-    if (!key) return;
+    if (key) {
+      const openaiInstance = new OpenAI({
+        apiKey: key,
+      });
 
-    const openaiInstance = new OpenAI({
-      apiKey: key,
-    });
+      setAi(openaiInstance);
+      return;
+    }
 
-    setAi(openaiInstance);
+    const res = await startOllama();
+    const ollamaInstance = await ollama();
+    setAi(ollamaInstance);
+    console.log(ollamaInstance);
+    setModel('llama2')
+    setType('ollama')
+    return;
+
+
   };
 
   const getKey = (accountName) => {
     return window.electron.ipc.invoke('get-ai-key');
+  };
+
+  const startOllama = () => {
+    return window.electron.ipc.invoke('start-ollama');
   };
 
   const setKey = (secretKey) => {
@@ -43,7 +62,7 @@ export const AIContextProvider = ({ children }) => {
     return window.electron.ipc.invoke('delete-ai-key');
   };
 
-  const getCompletion = async (model = 'gpt-3', context) => {
+  const getCompletion = async (context) => {
     const response = await ai.chat.completions.create({
       model: model,
       max_tokens: 200,
@@ -56,6 +75,8 @@ export const AIContextProvider = ({ children }) => {
   const AIContextValue = {
     ai,
     prompt,
+    model,
+    type,
     setKey,
     getKey,
     deleteKey,

3bc427f149dcdbb1e876c1e2aa39501ff893bbe5
udara <me@udara.io>
2024-01-03 21:11:17 -0500

    IPC -> keys: remove model handlers

--- src/main/handlers/keys.ts
+++ src/main/handlers/keys.ts
@@ -12,23 +12,3 @@ ipcMain.handle('set-ai-key', async (event, secretKey) => {
 ipcMain.handle('delete-ai-key', async () => {
   return await keytar.deletePassword('pile', 'aikey');
 });
-
-ipcMain.handle('get-model-name', async () => {
-  return await keytar.getPassword('pile', 'modelname');
-});
-
-ipcMain.handle('set-model-name', async (event, modelName) => {
-  return await keytar.setPassword('pile', 'modelname', modelName);
-});
-
-ipcMain.handle('clear-model-name', async () => {
-  return await keytar.deletePassword('pile', 'modelname');
-});
-
-ipcMain.handle('get-model-type', async () => {
-  return await keytar.getPassword('pile', 'modeltype');
-});
-
-ipcMain.handle('set-model-type', async (event, modelType) => {
-  return await keytar.setPassword('pile', 'modeltype', modelType);
-});
